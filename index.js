002498068067824502f80a1d612a530f3646bfcd
lucasbasombrio <basombrio.lucas5@gmail.com>
2025-02-10 01:03:11 -0300

    primera parte de clase11

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 import express from "express";
-import routes from "./routes/routes.js";
-import { errorNotFound } from "./middlewares/errorNotFound.js";
-import connectionDb from "./connection/connectionDb.js";
-import { SERVER_PORT } from "./config/config.js";
-import cookieParser from "cookie-parser";
-//import { roleSeed } from "./seed/roleSeed.js";
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser());
 
-app.use(routes);
 
-app.use(errorNotFound);
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-await connectionDb.sync({force:false}); //alter true para que haga el DDL y todo este actualizado
-                        //force:true es para que dropee y cree todo el tiempo
 
-                        //await roleSeed()
 
-app.listen(SERVER_PORT, () => {
-  console.log(`🚀 listening  ${SERVER_PORT}`);
-});
+app.listen(8080, ()=>{
+    console.log("🚀 ~ app.listen ~ 8080")
+    
+})
--- middlewares/methodLoger.js
+++
@@ -1,9 +0,0 @@
-export const methodLoger=(req, res, next)=>{       //middleware //exporto solo esta funcion
-    console.log("🚀 url:", req.url)
-    console.log("🚀 method:", req.method)
-    next();
-};  
-
-
-
-// export default methodLoger  //exporto todo el archivo
