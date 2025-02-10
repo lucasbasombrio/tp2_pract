dc0759a0b0b6f3e67069001a7109e793f8a0da21
lucasbasombrio <basombrio.lucas5@gmail.com>
2025-02-10 15:55:53 -0300

    modelo con Osvaldo

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
+import Controller from "../controller/Controller.js";
 
 const routes = Router();
+const controller = new Controller 
 
- routes.post("/")
+ routes.post("/", controller.ingresarPalabra)
 
- routes.get("/")
+ routes.get("/", controller.listarPalabras)
  routes.get("/all")
- routes.get("/:cantidad")
+ routes.get("/:cantidad", controller.allApiPalabras)
 
- routes.delete("/")
+ routes.delete("/:palabra", controller.borrarPalabras)
 
 export default routes;
